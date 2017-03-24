26c890ed2cef6f412d9c460ae3042f64374b1307
Vayne-Lover <406378362@qq.com>
2017-03-24 09:56:08 +0800

    Add more commands.

--- R in Action/chapter2/item4.R
+++ R in Action/chapter2/item4.R
@@ -3,4 +3,16 @@ age <- c(23,52,62,24)
 type <- c("Tpye1","Type2","Type1","Type1")
 status <- c("poor","improved","excellent","poor")
 data <- data.frame(ID,age,type,status)
-data
+#data[1:2]
+#data$age
+#data[c("ID","type")]
+#table(data$ID,data$status)
+#str(data)
+#summary(data)
+res <- c(1,2,3)
+res
+with(data,{
+  res <- summary(age)
+  res
+})
+
