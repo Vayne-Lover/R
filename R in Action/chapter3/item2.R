ff76dea9d5c73a131d8e1b82950edfa683c5b890
Vayne-Lover <406378362@qq.com>
2017-03-26 18:18:32 +0800

    Add item 2.

---
+++ R in Action/chapter3/item2.R
@@ -0,0 +1,9 @@
+a <- c(20,30,40,45,60)
+b <- c(16,20,27,40,60)
+c <- c(15,18,25,31,40)
+plot(a,b,type="b",col="red",
+     lty=2,pch=2,lwd=2,
+     main="First Plot",
+     sub="data",
+     xlab="a",ylab="b",
+     xlim=c(0,60),ylim=c(0,70))
